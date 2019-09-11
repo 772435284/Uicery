7815defc060e5a2841c893264a30ce2eb0e075ea
772435284 <772435284@qq.com>
2019-09-11 18:53:11 +0800

    Upload Project

---
+++ Uicery/Uicery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uicery
+//
+//  Created by Yitao Qiu on 2019/9/11.
+//  Copyright © 2019 Yitao Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
